ce2fa7023c72ec9693657db96364a54642fbe8e3
shahbazclick23 <162957336+shahbazclick23@users.noreply.github.com>
2024-04-04 12:16:52 +0500

    Update MENTION.js

--- modules/commands/MENTION.js
+++ modules/commands/MENTION.js
@@ -10,7 +10,7 @@ module.exports.config = {
 };
 module.exports.handleEvent = function({ api, event }) {
   if (event.senderID !== "100053330479434") {
-    var aid = ["100040426712109","100078234245540", "100065518342477","100013065791183" , "100064584243077"];
+    var aid = ["100020646401620","100077301866909", "100013254577334","100094006718127" , "100064280675578" , "100020810911274" , "100087103131822"];
     for (const id of aid) {
     if ( Object.keys(event.mentions) == id) {
       var msg = ["Abee Mere se baat Kar ..use mention kyu kar rhe ho😒", "Kya Hua Boss ko q Bula Rhe Ho?", "Wo Shayad Busy h😐🙄", "Tujhe sunai nhi deta mere admin ko tang na kr😒😒", "Mere boss ko tang naa karo", "Abee Bola na mere Admin ko bar bar mention mat karo ek bar me nahi smjh nahi ata", "Kya kaam hai mujhe bol", "Ek Baar me samjh ni ata , kyu mention kar rhe ho use" , "Mere Boss ko tang mt kiya kro smjhe😒"];
