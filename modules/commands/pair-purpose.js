23fc4fc0515e0344a0baec3ad55c770ae1859f72
shahbazclick23 <162957336+shahbazclick23@users.noreply.github.com>
2024-04-04 14:44:27 +0500

    Update pair-purpose.js

--- modules/commands/pair-purpose.js
+++ modules/commands/pair-purpose.js
@@ -20,9 +20,9 @@ module.exports.onLoad = async() => {
     const { existsSync, mkdirSync } = global.nodemodule["fs-extra"];
     const { downloadFile } = global.utils;
     const dirMaterial = __dirname + `/cache/canvas/`;
-    const path = resolve(__dirname, 'cache/canvas', 'lovep.png');
+    const path = resolve(__dirname, 'cache/canvas', 'lovepp2.png');
     if (!existsSync(dirMaterial + "canvas")) mkdirSync(dirMaterial, { recursive: true });
-    if (!existsSync(path)) await downloadFile("https://i.ibb.co/ct9Pwyf/image.jpg", path); 
+    if (!existsSync(path)) await downloadFile("https://i.imgur.com/GvrPI6v.jpeg", path); 
 }
 
 async function makeImage({ one, two }) {
@@ -32,7 +32,7 @@ async function makeImage({ one, two }) {
     const jimp = global.nodemodule["jimp"];
     const __root = path.resolve(__dirname, "cache", "canvas");
 
-    let batgiam_img = await jimp.read(__root + "/lovep.png");
+    let batgiam_img = await jimp.read(__root + "/lovepp2.png");
     let pathImg = __root + `/batman${one}_${two}.png`;
     let avatarOne = __root + `/avt_${one}.png`;
     let avatarTwo = __root + `/avt_${two}.png`;
