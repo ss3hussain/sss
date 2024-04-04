13f8f522ba42ea94bd710be6e204d3e1fc483f87
shahbazclick23 <162957336+shahbazclick23@users.noreply.github.com>
2024-04-04 14:38:05 +0500

    Update pair8.js

--- modules/commands/pair8.js
+++ modules/commands/pair8.js
@@ -20,7 +20,7 @@ module.exports.onLoad = async() => {
     const { existsSync, mkdirSync } = global.nodemodule["fs-extra"];
     const { downloadFile } = global.utils;
     const dirMaterial = __dirname + `/cache/canvas/`;
-    const path = resolve(__dirname, 'cache/canvas', 'ar2.png');
+    const path = resolve(__dirname, 'cache/canvas', 'pair8.png');
     if (!existsSync(dirMaterial + "canvas")) mkdirSync(dirMaterial, { recursive: true });
     if (!existsSync(path)) await downloadFile("https://i.imgur.com/mrLbvQW.jpg", path); 
 }
@@ -32,7 +32,7 @@ async function makeImage({ one, two }) {
     const jimp = global.nodemodule["jimp"];
     const __root = path.resolve(__dirname, "cache", "canvas");
 
-    let batgiam_img = await jimp.read(__root + "/ar2.png");
+    let batgiam_img = await jimp.read(__root + "/pair8.png");
     let pathImg = __root + `/batman${one}_${two}.png`;
     let avatarOne = __root + `/avt_${one}.png`;
     let avatarTwo = __root + `/avt_${two}.png`;
